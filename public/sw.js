12e853129739a6fcb900ac5a12099fc6db1c1f27
Ethereaa <120999276+Ethereaa@users.noreply.github.com>
2026-07-29 04:34:33 +0000

    Done Fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'agenda-kanwil-cache-v1';
-const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/kemenag.svg'];
+const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/kemenag-seeklogo.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
