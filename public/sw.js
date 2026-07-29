38d86c18aba1b5459951ee2373e6555ab8f1473e
Ethereaa <120999276+Ethereaa@users.noreply.github.com>
2026-07-29 14:46:05 +0800

    Update sw.js
    ok

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agenda-kanwil-cache-v1';
+const CACHE_NAME = 'agenda-kanwil-cache-v2';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/kemenag-seeklogo.svg'];
 
 self.addEventListener('install', (event) => {
@@ -7,7 +7,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
   self.clients.claim();
 });
 
