bcfadca9821b635208e2f14a78fc42591eb01e89
Ethereaa <120999276+Ethereaa@users.noreply.github.com>
2026-07-30 09:40:32 +0800

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,13 @@
-const CACHE_NAME = 'agenda-kanwil-cache-v2';
-const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/kemenag-seeklogo.svg'];
+const CACHE_NAME = 'agenda-kanwil-cache-v3';
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/kemenag-seeklogo.svg',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/icon-maskable-512.png',
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
